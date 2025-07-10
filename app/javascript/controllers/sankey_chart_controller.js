cd3d00db4efc8bf666efee078f61667a04e675db
Hendri Permana <hendri@permana.icu>
2025-07-11 00:26:06 +0700

    Backup before sync 20250711_002606

--- app/javascript/controllers/sankey_chart_controller.js
+++ app/javascript/controllers/sankey_chart_controller.js
@@ -38,9 +38,6 @@ export default class extends Controller {
       .attr("width", width)
       .attr("height", height);
 
-    // Container for all sankey elements, used for zooming
-    const chartGroup = svg.append("g").attr("class", "chartGroup");
-
     const sankeyGenerator = sankey()
       .nodeWidth(this.nodeWidthValue)
       .nodePadding(this.nodePaddingValue)
@@ -54,8 +51,8 @@ export default class extends Controller {
       links: links.map((d) => Object.assign({}, d)),
     });
 
-    // Define gradients
-    const defs = chartGroup.append("defs");
+    // Define gradients for links
+    const defs = svg.append("defs");
 
     sankeyData.links.forEach((link, i) => {
       const gradientId = `link-gradient-${link.source.index}-${link.target.index}-${i}`;
@@ -94,7 +91,7 @@ export default class extends Controller {
     });
 
     // Draw links
-    chartGroup
+    svg
       .append("g")
       .attr("fill", "none")
       .selectAll("path")
@@ -115,7 +112,7 @@ export default class extends Controller {
       .text((d) => `${nodes[d.source.index].name} → ${nodes[d.target.index].name}: ${this.currencySymbolValue}${Number.parseFloat(d.value).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} (${d.percentage}%)`);
 
     // Draw nodes
-    const node = chartGroup
+    const node = svg
       .append("g")
       .selectAll("g")
       .data(sankeyData.nodes)
@@ -203,22 +200,5 @@ export default class extends Controller {
         financialDetailsTspan.append("tspan")
           .text(stimulusControllerInstance.currencySymbolValue + Number.parseFloat(d.value).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
       });
-
-    // Enable zoom and pan
-    svg.call(
-      d3.zoom()
-        .scaleExtent([0.5, 3])
-        .on("zoom", (event) => chartGroup.attr("transform", event.transform))
-    );
   }
-
-  // Fullscreen toggle for chart container
-  toggleFullscreen() {
-    const el = this.element;
-    if (!document.fullscreenElement) {
-      el.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
-  }
-}
+} 
